b8a7512f299b603f4a5912393008a8bf44cacd32
Mudasrah-000 <124970212+Mudasrah-000@users.noreply.github.com>
2023-02-13 13:48:02 +0500

    Add files via upload

---
+++ work.py
@@ -0,0 +1,61 @@
+#sting
+y="mudasarah"
+print(y)
+
+
+#variable
+x= 5
+y= "sadaf"
+print(x,y)
+
+#list
+list=["amna", "mahi", "044"]
+print(list)
+
+#tuple 
+tuple = ("amna", "mahi", "044")
+print(tuple)
+
+#set
+set= {"amna", "mahi", "044"}
+print(set)
+
+#dictionaries
+dict = {
+    "name": "Mudasrah",
+        "roll no": 44
+        }
+print(dict)
+
+#if-else code 
+a= 200
+b= 44
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")   
+    
+#for loop
+fruits= ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    
+#while loop
+i= 2
+while i < 7:
+    print(i)
+    if i == 3:
+        break
+    i+= 3
+    
+#function 
+def my_function():
+    print("i am student at IUB")     
+my_function()
+
+
+
+
+
+
+
